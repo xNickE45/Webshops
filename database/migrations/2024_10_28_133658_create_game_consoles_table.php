c39b116f580d3b28043b790b17ca89513bad7c65
xNickE45 <159444109+xNickE45@users.noreply.github.com>
2024-10-29 13:58:26 +0100

    migrations added

---
+++ database/migrations/2024_10_28_133658_create_game_consoles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_consoles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image_url');
+            $table->decimal('price');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game_consoles');
+    }
+};
